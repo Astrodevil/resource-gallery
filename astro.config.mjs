e1cdff5055df3edd9d2db64a146909e6bd91da02
Mr. Ånand <anandamitesh0812@gmail.com>
2023-01-22 21:39:50 +0530

    update

---
+++ astro.config.mjs
@@ -0,0 +1,17 @@
+import { defineConfig } from "astro/config";
+import tailwind from "@astrojs/tailwind";
+import image from "@astrojs/image";
+import mdx from "@astrojs/mdx";
+import sitemap from "@astrojs/sitemap";
+
+export default defineConfig({
+  site: "https://astroship.web3templates.com",
+  integrations: [
+    tailwind(),
+    image({
+      serviceEntryPoint: "@astrojs/image/sharp",
+    }),
+    mdx(),
+    sitemap(),
+  ],
+});
