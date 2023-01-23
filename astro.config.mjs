de00f5a72ae76e2f6c524b202e9379786d23d77d
Mr. Ånand <anandamitesh0812@gmail.com>
2023-01-23 11:40:23 +0530

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,7 @@ import tailwind from "@astrojs/tailwind";
 import image from "@astrojs/image";
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
+import netlify from '@astrojs/netlify/functions';
 
 export default defineConfig({
   site: "https://astroship.web3templates.com",
@@ -14,4 +15,6 @@ export default defineConfig({
     mdx(),
     sitemap(),
   ],
+  output: 'server',
+  adapter: netlify(),
 });
