2434b8fc40d58a0de894cf17f8ebb9bab1b7c736
Mr. Ånand <anandamitesh0812@gmail.com>
2023-01-24 00:37:25 +0530

    push

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,7 @@ import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
 
 export default defineConfig({
-  site: "https://astroship.web3templates.com",
+  site: "https://resource-gallery.pages.dev/",
   integrations: [
     tailwind(),
     image({
