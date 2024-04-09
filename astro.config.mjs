2d97406b7d7ca063febcbb7473935e44a8743682
Mr. Ånand <anandamitesh0812@gmail.com>
2024-04-10 02:58:34 +0530

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,22 +1,18 @@
-import { defineConfig } from 'astro/config';
-import tailwind from '@astrojs/tailwind';
-import image from '@astrojs/image';
-import mdx from '@astrojs/mdx';
-import sitemap from '@astrojs/sitemap';
+import { defineConfig } from "astro/config";
+import tailwind from "@astrojs/tailwind";
+import image from "@astrojs/image";
+import mdx from "@astrojs/mdx";
+import sitemap from "@astrojs/sitemap";
 
 export default defineConfig({
-  site: 'https://resourcegallery.live/',
+  site: "https://resourcegallery.live/",
   integrations: [
     tailwind(),
     image({
-      serviceEntryPoint: '@astrojs/image/sharp',
+      serviceEntryPoint: "@astrojs/image/sharp",
     }),
     mdx(),
     sitemap(),
   ],
-  vite: {
-    optimizeDeps: {
-      include: ['./assets/services/noop'], // Replace with the actual module name causing the issue
-    },
-  },
+
 });
